e0ec5804507c238a3cfb99f26df31096ed957616
WilliamAbraham <williamjohnabraham1@gmail.com>
2023-10-18 17:58:08 -0700

    please

--- .utils/firebase.js
+++ .utils/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdypcPRu6twSJvhDj0hnkEbbzHWOhHdTM",
-  authDomain: "williams-project-eb009.firebaseapp.com",
-  projectId: "williams-project-eb009",
-  storageBucket: "williams-project-eb009.appspot.com",
-  messagingSenderId: "447513020809",
-  appId: "1:447513020809:web:e24285a9a8f13a76eaf958",
-  measurementId: "G-6SRQJP7LHV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
